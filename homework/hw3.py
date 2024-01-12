d18e687f886331d1753a9168a506b31001794929
nelson023 <84180654+nelson023@users.noreply.github.com>
2024-01-13 02:27:45 +0800

    Update hw3.py

--- homework/hw3.py
+++ homework/hw3.py
@@ -1,1 +1,21 @@
-print('hw3')
+def generate_permutations(n):
+    result = []  
+
+    def perm_next(p):
+        i = len(p)
+        if i == n:
+            result.append(p.copy())  
+            return
+        for x in range(n):
+            if x not in p:
+                p.append(x)
+                perm_next(p)
+                p.pop()
+
+    perm_next([])
+    return result
+
+print(generate_permutations(2))
+print(generate_permutations(3))
+print(generate_permutations(4))
+print(generate_permutations(5))
